f9ecf871d94bee979c55b79b0e36f9a16be23999
katherinengggg <e1112901@u.nus.edu>
2023-06-21 00:18:53 +0800

    home page design

--- src/App.js
+++ src/App.js
@@ -14,15 +14,38 @@ function App() {
   const name = "visitor";
 
   return (
-    <div>
-      <div>
-        <p>Greetings {name}!</p>
-        <p>Here are some courses we have available at our distinguished institution.</p>
-      </div>
-      <h2>Available Courses:</h2>
-      <ListCourseApi />    
-      <SuggestTag />  
-    </div>
+    <section className='p-5 bg-light'>
+      <section className='bg-secondary p-3'>
+        <div className='container'>
+          <h2 className='text-info text-center'><b>Greetings {name}!</b></h2>
+          <p className='text-white mt-3 text-justify'><b>We are dedicated to providing a transformative educational experience that fosters intellectual growth, advances knowledge, and inspires innovation. Through rigorous academic programs and interdisciplinary approaches, we equip students with the skills and competencies needed for success in a changing global society. 
+            <br /><br />Here are some courses we have available at our distinguished institution.</b></p>
+        </div>
+      </section>
+
+      <section className='bg-white p-5'>
+        <div className='container text-center'>
+          <h2 className='align-text-center text-secondary'>Available Courses</h2>
+          <div>
+            <SuggestTag />
+          </div>
+          <div>
+            <ListCourseApi />
+          </div>
+        </div>
+      </section>
+      
+    </section>
+
+    // <div>
+    //   <div>
+    //     <p>Greetings {name}!</p>
+    //     <p>Here are some courses we have available at our distinguished institution.</p>
+    //   </div>
+    //   <h2>Available Courses:</h2>
+    //   <ListCourseApi />    
+    //   <SuggestTag />  
+    // </div>
   );
 }
 
--- src/SuggestTag.js
+++ src/SuggestTag.js
@@ -22,12 +22,28 @@ export default function SuggestTag(){
     }
     return (
         <div>
-            <h4>Suggest a Topic You'd Love to See Courses For:</h4>
-            <form>
-                <label htmlFor='tagInfo'>Topic Tag: </label>
-                <input type='test' name='tagInfo' ref={tagElement}/><br /><br />
-                <button onClick ={handleCreateClick}>Submit</button>
-            </form>
+            <div class="mb-3 mt-3">
+                <input class="form-control" list="datalistOptions" id="exampleDataList" type='test' name='tagInfo' ref={tagElement} placeholder="Suggest a Topic You'd Love to See Courses for..."/>
+                <datalist id="datalistOptions">
+                    <option value="Java Spring"/>
+                    <option value="C Sharp"/>
+                    <option value="Machine Learning"/>
+                </datalist>
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-secondary mt-3 mb-3" onClick ={handleCreateClick}>Submit</button>
+                </div>
+
+            </div>
         </div>
+
+
+        // <div>
+        //     <h4>Suggest a Topic You'd Love to See Courses For:</h4>
+        //     <form>
+        //         <label htmlFor='tagInfo'>Topic Tag: </label>
+        //         <input type='test' name='tagInfo' ref={tagElement}/><br /><br />
+        //         <button onClick ={handleCreateClick}>Submit</button>
+        //     </form>
+        // </div>
     )
 }
