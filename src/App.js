b2aee0d13ce44142d524c59b5a79f13104a4d0b7
DESKTOP-733BC02\TRN <jason.tay.nw@gmail.com>
2023-06-21 12:12:37 +0800

    updated for data feedback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-//ref:
-  //https://www.youtube.com/watch?v=I1IH96LzDNc&t=49s
-  //https://www.youtube.com/watch?v=BeiRU10gzfI&t=2382s   //use this
-
 import React, {useEffect, useState} from 'react';
 import ListCourseApi from './ListCourseApi';
 import SuggestTag from './SuggestTag';
--- src/SuggestTag.js
+++ src/SuggestTag.js
@@ -1,8 +1,9 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 import axios from 'axios';
 
 export default function SuggestTag(){
 
+    const [responseObj, updateResponseObj] = useState(null);
     const tagElement = useRef(null);
 
     function handleCreateClick(e) {
@@ -15,18 +16,40 @@ export default function SuggestTag(){
         axios
             .post("http://localhost:4000/api/tags", data)
             .then(response=>{
+                updateResponseObj(response.data);
                 console.log("Success!");
                 console.log(response.data);
             })
             .catch(e=> {console.log(e);})
+
+        
     }
+
+    function showCreated(){
+        if(responseObj) {
+            //check response.data
+            //could also checkhttp response?
+                //if empty string or object, do not print
+            return(
+                <div>
+                    <div>We've created a subject tag for your suggestion: {responseObj.tagInfo}</div>
+                </div>
+            )
+        }
+        else{
+            return(<></>);
+        }
+
+    }
+
     return (
         <div>
             <h4>Suggest a Topic You'd Love to See Courses For:</h4>
             <form>
                 <label htmlFor='tagInfo'>Topic Tag: </label>
                 <input type='test' name='tagInfo' ref={tagElement}/><br /><br />
-                <button onClick ={handleCreateClick}>Submit</button>
+                <button onClick ={handleCreateClick}>Submit</button> <br /><br />
+                {showCreated()}
             </form>
         </div>
     )
